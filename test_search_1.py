8ba743d0d679cedfaccb3a6255001135e6c26b1b
Valentin Konuykh <konyuh.ktsstudio@gmail.com>
2024-09-23 20:20:28 +0300

    подправил форматирование

--- test_search_1.py
+++ test_search_1.py
@@ -4,6 +4,8 @@ from selene import browser, be, have
 def test_google_search_1(setup_browser):
     browser.element('[name="q"]').should(be.visible).should(be.blank).type('yashaka/selene').press_enter()
     browser.element('[id="botstuff"]').should(have.text('yashaka/selene: User-oriented Web UI browser tests'))
+
+
 def test_search_google_2_invalid(setup_browser, check_results):
-    browser.element(('[name="q"]')).should(be.visible).should(be.blank).type('@@@98342g+3y89438+25b').press_enter()
+    browser.element('[name="q"]').should(be.visible).should(be.blank).type('@@@98342g+3y89438+25b').press_enter()
     browser.element('[id="botstuff"]').should(have.text('ничего не найдено'))
